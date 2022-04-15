050bd6f207e4709a5c4ee82bb9e75e416eceb2bd
Bas Pennings <baspster@gmail.com>
2022-04-15 14:05:26 +0200

    Reorganize some affine code

--- src/Linie/Affine.cs
+++ src/Linie/Affine.cs
@@ -93,61 +93,6 @@ public static class Affine
 
         return orientation * Translate(-from.X, -from.Y, -from.Z);
     }
-
-    /// <summary>
-    /// Applies a translation matrix to an existing matrix.
-    /// </summary>
-    public static Matrix4x4 Translate(
-        this Matrix4x4 m,
-        double x,
-        double y,
-        double z) => Affine.Translate(x, y, z) * m;
-
-    /// <summary>
-    /// Applies a scale matrix to an existing matrix using 
-    /// constant scale factor in all dimensions.
-    /// </summary>
-    public static Matrix4x4 Scale(this Matrix4x4 m, double s) =>
-        Scale(m, s, s, s);
-
-    /// <summary>
-    /// Applies a scale matrix to an existing matrix.
-    /// </summary>
-    public static Matrix4x4 Scale(
-        this Matrix4x4 m,
-        double x,
-        double y,
-        double z) => Affine.Scale(x, y, z) * m;
-
-    /// <summary>
-    /// Applies a x-axis rotation matrix to a given matrix.
-    /// </summary>
-    public static Matrix4x4 RotateX(this Matrix4x4 m, double r) =>
-        Affine.RotateX(r) * m;
-
-    /// <summary>
-    /// Applies an y-axis rotation matrix to a given matrix.
-    /// </summary>
-    public static Matrix4x4 RotateY(this Matrix4x4 m, double r) =>
-        Affine.RotateY(r) * m;
-
-    /// <summary>
-    /// Applies a z-axis rotation matrix to a given matrix.
-    /// </summary>
-    public static Matrix4x4 RotateZ(this Matrix4x4 m, double r) =>
-        Affine.RotateZ(r) * m;
-
-    /// <summary>
-    /// Applies a shear matrix to a given matrix.
-    /// </summary>
-    public static Matrix4x4 Shear(
-        this Matrix4x4 m,
-        double xy,
-        double xz,
-        double yx,
-        double yz,
-        double zx,
-        double zy) => Affine.Shear(xy, xz, yx, yz, zx, zy) * m;
 }
 
 #pragma warning restore SA1117 // ParametersMustBeOnSameLineOrSeparateLines
--- src/Linie/Matrix4x4.cs
+++ src/Linie/Matrix4x4.cs
@@ -229,4 +229,60 @@ public static class Matrix4x4Extensions
 
     internal static double Cofactor(this Matrix4x4 a, int row, int col) =>
         (row + col) % 2 == 0 ? a.Minor(row, col) : -a.Minor(row, col);
+
+
+    /// <summary>
+    /// Applies a translation matrix to an existing matrix.
+    /// </summary>
+    public static Matrix4x4 Translate(
+        this Matrix4x4 m,
+        double x,
+        double y,
+        double z) => Affine.Translate(x, y, z) * m;
+
+    /// <summary>
+    /// Applies a scale matrix to an existing matrix using 
+    /// constant scale factor in all dimensions.
+    /// </summary>
+    public static Matrix4x4 Scale(this Matrix4x4 m, double s) =>
+        Scale(m, s, s, s);
+
+    /// <summary>
+    /// Applies a scale matrix to an existing matrix.
+    /// </summary>
+    public static Matrix4x4 Scale(
+        this Matrix4x4 m,
+        double x,
+        double y,
+        double z) => Affine.Scale(x, y, z) * m;
+
+    /// <summary>
+    /// Applies a x-axis rotation matrix to a given matrix.
+    /// </summary>
+    public static Matrix4x4 RotateX(this Matrix4x4 m, double r) =>
+        Affine.RotateX(r) * m;
+
+    /// <summary>
+    /// Applies an y-axis rotation matrix to a given matrix.
+    /// </summary>
+    public static Matrix4x4 RotateY(this Matrix4x4 m, double r) =>
+        Affine.RotateY(r) * m;
+
+    /// <summary>
+    /// Applies a z-axis rotation matrix to a given matrix.
+    /// </summary>
+    public static Matrix4x4 RotateZ(this Matrix4x4 m, double r) =>
+        Affine.RotateZ(r) * m;
+
+    /// <summary>
+    /// Applies a shear matrix to a given matrix.
+    /// </summary>
+    public static Matrix4x4 Shear(
+        this Matrix4x4 m,
+        double xy,
+        double xz,
+        double yx,
+        double yz,
+        double zx,
+        double zy) => Affine.Shear(xy, xz, yx, yz, zx, zy) * m;        
 }
